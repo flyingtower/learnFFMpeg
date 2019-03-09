2b15c50a406d153e02f88fc7c835f0bfe48efae0
fengtao <fengtao@qiushibaike.com>
2019-03-09 20:33:20 +0800

    Initial Commit

---
+++ myLive/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  myLive
+//
+//  Created by yeguo on 2019/3/9.
+//  Copyright © 2019年 yeguo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
